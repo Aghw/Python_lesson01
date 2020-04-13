9716c893cb395ac35fa969954105091c34036ba1
Aghw <sam.asmerom@outlook.com>
2020-04-12 19:37:56 -0700

    I added comments to the functions in the files

--- warmup_1.py
+++ warmup_1.py
@@ -1,38 +1,135 @@
 #!/usr/bin/env python
 
+'''
+The parameter weekday is True if it is a weekday, and the parameter vacation is
+True if we are on vacation. We sleep in if it is not a weekday or we're on vacation.
+Return True if we sleep in
+'''
+
 def sleep_in(weekday, vacation):
-    return (not weekday or vacation)
+    '''
+    sleep-in function
+    '''
+    return not weekday or vacation
 
-
+'''
+We have two monkeys, a and b, and the parameters a_smile and b_smile
+indicate if each is smiling. We are in trouble if they are both smiling or if
+neither of them is smiling. Return True if we are in trouble.
+'''
 def monkey_trouble(a_smile, b_smile):
-    return not (a_smile ^ b_smile)
+    '''
+    monkey-trouble function
+    '''
+    return not a_smile ^ b_smile
 
+'''
+Given two int values, return their sum. Unless the two values are the same,
+then return double their sum.
+'''
 def sum_double(a, b):
-    return (a + b if a != b else 4 * a)
+    '''
+    sum-double function
+    '''
+    return a + b if a != b else 4 * a
 
+'''
+Given an int n, return the absolute difference between n and 21, except return
+double the absolute difference if n is over 21.
+'''
 def diff21(n):
-    return (2 * (n-21) if n > 21 else 21 - n)
+    '''
+    difference-21 function
+    '''
+    return 2 * (n-21) if n > 21 else 21 - n
 
+'''
+We have a loud talking parrot. The "hour" parameter is the current hour time in
+the range 0..23. We are in trouble if the parrot is talking and the hour is
+before 7 or after 20. Return True if we are in trouble.
+'''
 def parrot_trouble(talking, hour):
-    return  (talking and (hour < 7 or hour > 20))
+    '''
+    parrot-trouble function
+    '''
+    return talking and (hour < 7 or hour > 20)
 
+'''
+Given 2 ints, a and b, return True if one if them is 10 or if their sum is 10.
+'''
 def makes10(a, b):
-    return (10 == a or 10 == b or 10 == (a + b))
+    '''
+    makes-10 function
+    '''
+    return 10 == a or 10 == b or 10 == (a + b)
 
+
+'''
+Given an int n, return True if it is within 10 of 100 or 200. Note: abs(num)
+computes the absolute value of a number.
+'''
 def near_hundred(n):
-    return (abs(n-100) <= 10 or abs(n-200) <= 10)
+    '''
+    near-hundred function
+    '''
+    return abs(n-100) <= 10 or abs(n-200) <= 10
 
+
+'''
+Given 2 int values, return True if one is negative and one is positive.
+Except if the parameter "negative" is True, then return True only if
+both are negative.
+'''
 def pos_neg(a, b, negative):
-    return (negative and (a < 0) and (b < 0) or (not negative and (a >= 0 and b < 0 or b >= 0 and a < 0)))
+    '''
+    positive-negative function
+    '''
+    if negative and (a < 0) and (b < 0):
+        return True
+    elif (not negative and (a >= 0 and b < 0 or b >= 0 and a < 0)):
+        return True
+    return False
 
+'''
+Given a string, return a new string where "not " has been added to the front.
+However, if the string already begins with "not", return the string unchanged.
+'''
 def not_string(str):
-    return (str if str[0:3] == 'not' else 'not ' + str)
+    '''
+    not-string function
+    '''
+    return str if str[0:3] == 'not' else 'not ' + str
 
+
+'''
+Given a non-empty string and an int n, return a new string where the char at
+index n has been removed. The value of n will be a valid index of a char in
+the original string (i.e. n will be in the range 0..len(str)-1 inclusive).
+'''
 def missing_char(str, n):
+    '''
+    missing-char function
+    '''
     return str[:n] + str[n + 1: ]
 
+
+'''
+Given a string, return a new string where the first and last chars have been exchanged.
+'''
 def front_back(str):
+    '''
+    front-back function
+    '''
     return (str if len(str) == 1 else str[-1:] + str[1:-1] + str[:1])
 
+
+'''
+Given a string, we'll say that the front is the first 3 chars of the string.
+If the string length is less than 3, the front is whatever is there.
+Return a new string which is 3 copies of the front.
+'''
 def front3(str):
+    '''
+    front-3 function
+    '''
     return (str + str + str if len(str) < 3 else str[0:3] + str[0:3] + str[0:3])
--- warmup_2.py
+++ warmup_2.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python
 
+'''
+Given a string and a non-negative int n, return a larger
+string that is n copies of the original string.
+'''
 def string_times(str, n):
     temp = ''
     for i in range(n):
         temp += str
     return temp
 
+'''
+Given a string and a non-negative int n, we'll say that the front
+of the string is the first 3 chars, or whatever is there if
+the string is less than length 3. Return n copies of the front;
+'''
 def front_times(str, n):
     temp = str if len(str) < 3 else str[0:3]
     ret = ''
@@ -13,7 +22,10 @@ def front_times(str, n):
         ret += temp
     return ret
 
-
+'''
+Given a string, return a new string made of every
+other char starting with the first, so "Hello" yields "Hlo".
+'''
 def string_bits(str):
     start_str = str[:1]
     new_str = str[1:]
@@ -26,42 +38,66 @@ def string_bits(str):
             start_str += new_str[i]
     return start_str
 
+'''
+Given a non-empty string like "Code" return a string like "CCoCodCode".
+'''
 def string_splosion(str):
     temp = ''
-    while (len(str) > 0):
+    while len(str) > 0:
         temp = str + temp
         str = str[:-1]
     return temp
 
-
+'''
+Given a string, return the count of the number of times that a
+substring length 2 appears in the string and also as the last 2
+chars of the string, so "hixxxhi" yields 1
+(we won't count the end substring).
+'''
 def last2(str):
     counter = 0
     temp = str[-2:]
     new_str = str[:-1]
     for i in range(len(new_str)):
-        if new_str[i:i+2] == temp :
+        if new_str[i:i+2] == temp:
             counter += 1
     return counter
 
+'''
+Given an array of ints, return the number of 9's in the array.
+'''
 def array_count9(nums):
     return nums.count(9)
 
+'''
+Given an array of ints, return True if one of the first 4 elements
+in the array is a 9. The array length may be less than 4.
+'''
 def array_front9(nums):
     return nums[:4].count(9) > 0
 
+'''
+Given an array of ints, return True if the sequence of
+numbers 1, 2, 3 appears in the array somewhere.
+'''
 def array123(nums):
-    temp = [1,2,3]
+    temp = [1, 2, 3]
     new_nums = nums[:-2]
     for i in range(len(new_nums)):
-        if nums[i:i+3] == temp :
+        if nums[i:i+3] == temp:
             return True
     return False
 
+'''
+Given 2 strings, a and b, return the number of the positions
+where they contain the same length 2 substring. So "xxcaazz"
+and "xxbaaz" yields 3, since the "xx", "aa", and "az" substrings
+appear in the same place in both strings.
+'''
 def string_match(a, b):
     counter = 0
     a_sub = a[:-1]
-    b_sub = b[:-1]
     for i in range(len(a_sub)):
-        if b[i:i+2] == a[i:i+2] :
+        if b[i:i+2] == a[i:i+2]:
             counter += 1
     return counter
